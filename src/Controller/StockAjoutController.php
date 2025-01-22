30b588245e8556cff584aff27bf03217b7966ade
kaira <husaynmozart@gmail.com>
2025-01-22 12:13:11 +0300

    mise en place mise en forme date et bouton récue dans stock

--- src/Controller/StockAjoutController.php
+++ src/Controller/StockAjoutController.php
@@ -6,10 +6,14 @@ use App\Entity\Stock;
 use App\Form\StockType;
 use App\Form\AjoutStockType;
 use App\Entity\Mouvement;
+use App\Entity\Unite;
 use App\Form\MouvementType;
 use App\Form\AjoutMouvementType;
 use App\Repository\MaterielRepository;
+use App\Repository\StockRepository;
 use App\Repository\TypeMouvementRepository;
+use App\Repository\UniteRepository;
+use App\Repository\ConversionRepository;
 use App\Repository\EtatRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -29,7 +33,7 @@ class StockAjoutController extends AbstractController
     }
 
     #[Route('/stock/ajout/new', name: 'app_stock_ajout_new', methods: ['GET', 'POST'])]
-    public function stock_ajout(Request $request, EntityManagerInterface $entityManager, TypeMouvementRepository $typeMouvementRepository, EtatRepository $etatRepository): Response
+    public function stock_ajout(Request $request, ConversionRepository $conversionRepository, EntityManagerInterface $entityManager, TypeMouvementRepository $typeMouvementRepository, EtatRepository $etatRepository, StockRepository $stockRepository): Response
     {
         // fonction manao ajout entana amin'ny destinataire no eto
         $mouvement = new Mouvement();
@@ -45,6 +49,29 @@ class StockAjoutController extends AbstractController
             $mouvement->setType($type);
             $mouvement->setEtat($etat);
             $entityManager->persist($mouvement);
+
+            $stock = $stockRepository->findOneBy(["produit" => $mouvement->getProduit(), "service" => $mouvement->getService()]);
+            // mila atao ny unité farany ambany ny ato anaty gestion de stock
+            // izany hoe atao recherche ny unité farany ambany sy ny conversion izany
+            $quantite = $mouvement->getQuantite();
+            $unite = $mouvement->getUnite();
+            while ($conversionRepository->findOneBy(["source" => $unite, "produit" => $mouvement->getProduit()])){
+                $conversion = $conversionRepository->findOneBy(["source" => $unite, "produit" => $mouvement->getProduit()]);
+                $quantite = $quantite * $conversion->getQuantite();
+                $unite = $conversion->getDestinataire();
+            }
+            if($stock == null){
+                $stock = new Stock();
+                $stock->setProduit($mouvement->getProduit());
+                $stock->setQuantite($quantite);
+                $stock->setUnite($unite);
+                $stock->setService($mouvement->getService());
+                $stock->setDate(new DateTime());
+            } else {
+                $stock->setQuantite($stock->getQuantite() + $quantite);
+                $stock->setDate(new DateTime());
+            }
+            $entityManager->persist($stock);
             $entityManager->flush();
 
             return $this->redirectToRoute('materiel_service', [], Response::HTTP_SEE_OTHER);
