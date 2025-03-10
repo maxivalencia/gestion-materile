b81d50f4708a4ae0d9aa04f8ded929d6e7de1129
kaira <husaynmozart@gmail.com>
2025-03-10 13:54:32 +0300

    mise en forme des dates

--- src/Form/CommandeStockType.php
+++ src/Form/CommandeStockType.php
@@ -19,7 +19,11 @@ class CommandeStockType extends AbstractType
                 'html5' => true,           // Active le calendrier HTML5 natif
                 'format' => 'yyyy-MM-dd',  // Format de la date (requis pour certains navigateurs)
             ])
-            ->add('expiration')
+            ->add('expiration', DateType::class, [
+                'widget' => 'single_text', // Utilise un input de type date
+                'html5' => true,           // Active le calendrier HTML5 natif
+                'format' => 'yyyy-MM-dd',  // Format de la date (requis pour certains navigateurs)
+            ])
             ->add('produit')
             ->add('unite')
             ->add('service')
